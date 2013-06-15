b651ea5d510a88d6560eba24b4826be79620668b
Scott Miao <jazzymiao@gmail.com>
2013-06-15 12:56:09 +0800

    Getting Started with Node.js

---
+++ Chapter2/helloweb.js
@@ -0,0 +1,9 @@
+var http = require("http");
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<html>");
+    response.write("<head><title>Node.js</title></head>");
+    response.write("<body>Hello Web</body>");
+    response.write("</html>");
+    response.end();
+}).listen(9999);
