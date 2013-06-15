b2317e3f10315e18caeac641e30c39cc08ade992
Scott Miao <jazzymiao@gmail.com>
2013-06-15 19:23:51 +0800

    The client

--- Chapter3/awesome-chat/app.js
+++ Chapter3/awesome-chat/app.js
@@ -10,7 +10,7 @@
  , path = require('path');
 
  var app = express();
-require('./routes/sockets.js').initialize(server);
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -30,15 +30,11 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-// http.createServer(app).listen(app.get('port'), function(){
-//   console.log('Express server listening on port ' + app.get('port'));
-// });
-
 var server = http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
 });
 
-
+require('./routes/sockets.js').initialize(server);
 
 
 
